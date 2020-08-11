81d129481a703f591ddebbf9391d29722b29f623
Vladimir Iglovikov <iglovikov@gmail.com>
2020-08-10 20:00:42 -0700

    fix in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"iglovikov@gmail.com\"\n\
+email = \”your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
